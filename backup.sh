ce310f3f07ee8a0730c8be681acc9281c7df4d2f
David Darke <david@atomicsmash.co.uk>
2015-04-10 13:01:21 +0100

    Folder tweaks

--- backup.sh
+++ backup.sh
@@ -3,6 +3,7 @@ HN=`hostname | awk -F. '{print $1}'`
  
 # Set variables
 DB_BACKUP="backups/$HN-`date +%d-%m-%Y_%H-%M`"
+DB_BACKUP_DROP="$HN-`date +%d-%m-%Y_%H-%M`"
 DB_USER="root"
 DB_PASSWD="root"
 
@@ -10,7 +11,7 @@ DB_PASSWD="root"
 mkdir -p $DB_BACKUP
  
 # Run through each database
-for db in $(/Applications/MAMP/Library/bin/mysql --user=$DB_USER --password=$DB_PASSWD -e 'show databases' -s --skip-column-names|grep -vi information_schema);
+for db in $(mysql --user=$DB_USER --password=$DB_PASSWD -e 'show databases' -s --skip-column-names|grep -vi information_schema);
 do
  
 # Check to make sure performance_schema is missed out
@@ -19,13 +20,13 @@ then
  
 # Then export
 echo "Exporting $db"
-/Applications/MAMP/Library/bin/mysqldump --user=$DB_USER --password=$DB_PASSWD --opt $db | gzip > "$DB_BACKUP/mysqldump-$HN-$db-$(date +%Y-%m-%d).gz";
+mysqldump --user=$DB_USER --password=$DB_PASSWD --opt $db | gzip > "$DB_BACKUP/mysqldump-$HN-$db-$(date +%Y-%m-%d).gz";
 fi;
  
 done
 
 echo "Export complete"
 
-./dropbox_uploader.sh upload $DB_BACKUP $DB_BACKUP
+./dropbox_uploader.sh upload $DB_BACKUP $DB_BACKUP_DROP
 
 echo "Upload complete"
